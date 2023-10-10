c718fbbbd12f6f43ab5e44a689c05dca31cd7f27
Rafael Reis <rafa.araujoreis@gmail.com>
2023-10-10 19:26:39 -0300

    criacao do projeto

---
+++ src/main/java/br/com/devfullstack/apitodolist/ApiTodoListApplication.java
@@ -0,0 +1,12 @@
+package br.com.devfullstack.apitodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTodoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTodoListApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/devfullstack/apitodolist/ApiTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devfullstack.apitodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
