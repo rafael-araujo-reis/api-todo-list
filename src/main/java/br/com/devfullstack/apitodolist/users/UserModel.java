290a77e56cb43c7b2c3d8a284f975c0046e6659a
Rafael Reis <rafa.araujoreis@gmail.com>
2023-10-10 21:26:45 -0300

    integrar com banco de dados e validar dados

--- src/main/java/br/com/devfullstack/apitodolist/users/UserModel.java
+++ src/main/java/br/com/devfullstack/apitodolist/users/UserModel.java
@@ -1,7 +1,29 @@
 package br.com.devfullstack.apitodolist.users;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity(name = "tb_users")
 public class UserModel {
-    public String name;
-    public String username;
-    public String password;
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(unique = true)
+    private String username;
+
+    private String name;
+    private String password;
 }
