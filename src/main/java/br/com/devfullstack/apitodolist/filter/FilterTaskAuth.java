41deed170166f1ca7bfb0ce273fbbe5eb9f74e74
Rafael Reis <rafa.araujoreis@gmail.com>
2023-10-11 22:05:01 -0300

    autenticação para criar tarefa

---
+++ src/main/java/br/com/devfullstack/apitodolist/filter/FilterTaskAuth.java
@@ -0,0 +1,50 @@
+package br.com.devfullstack.apitodolist.filter;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.devfullstack.apitodolist.users.IUserRepository;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Base64;
+
+@Component
+public class FilterTaskAuth extends OncePerRequestFilter {
+
+    @Autowired
+    private IUserRepository userRepository;
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        //Pegar dados da autenticação
+        var authorization = request.getHeader("Authorization").substring("Basic".length()).trim();
+        byte[] authDecode = Base64.getDecoder().decode(authorization);
+
+        String[] credentials = new String(authDecode).split(":");
+        String username = credentials[0];
+        String password = credentials[1];
+
+        //Valida usuário
+        var user = this.userRepository.findByUsername(username);
+
+        if(user == null){
+            response.sendError(401);
+            return;
+        }
+
+        //Valida senha
+        var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+
+        if(!passwordVerify.verified){
+            response.sendError(401);
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
