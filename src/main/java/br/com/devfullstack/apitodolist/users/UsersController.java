2169da2b6293dd349a1d38bc02a24358a7e34484
Rafael Reis <rafa.araujoreis@gmail.com>
2023-10-11 22:04:33 -0300

    criptografando a senha

--- src/main/java/br/com/devfullstack/apitodolist/users/UsersController.java
+++ src/main/java/br/com/devfullstack/apitodolist/users/UsersController.java
@@ -1,5 +1,6 @@
 package br.com.devfullstack.apitodolist.users;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +23,9 @@ public class UsersController {
         if (user != null) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existente");
         }
+
+        var passwordHash = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+        userModel.setPassword(passwordHash);
 
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
