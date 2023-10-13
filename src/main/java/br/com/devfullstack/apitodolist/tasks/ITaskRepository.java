8ea678e1151bcecc59e3f5be65a673073b1ea606
Rafael Reis <rafa.araujoreis@gmail.com>
2023-10-13 10:52:45 -0300

    busca pelo id do usuario

--- src/main/java/br/com/devfullstack/apitodolist/tasks/ITaskRepository.java
+++ src/main/java/br/com/devfullstack/apitodolist/tasks/ITaskRepository.java
@@ -1,7 +1,10 @@
 package br.com.devfullstack.apitodolist.tasks;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 import java.util.UUID;
 
 public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
+    List<TaskModel> findByUserId(UUID userId);
 }
